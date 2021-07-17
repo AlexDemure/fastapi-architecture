421a1c10b51e1d03b51adfe8b9b96a7c7214fba5
Alexander Grishchenko <alexanderdemure@gmail.com>
2021-07-17 18:42:05 +0500

    Delete venv directory

--- venv/Lib/site-packages/tortoise/contrib/mysql/fields.py
+++
@@ -1,5 +0,0 @@
-from tortoise.fields import Field
-
-
-class GeometryField(Field):
-    SQL_TYPE = "GEOMETRY"
--- venv/Lib/site-packages/tortoise/contrib/postgres/fields.py
+++
@@ -1,5 +0,0 @@
-from tortoise.fields import Field
-
-
-class TSVectorField(Field):
-    SQL_TYPE = "TSVECTOR"
